38ac699f47e6d660771dc60bf52e350e71349d4e
civic <tsskmail@gmail.com>
2021-01-13 18:30:34 +0900

    ライブラリ更新とヘッダ値取得修正

--- fs/init.js
+++ fs/init.js
@@ -2,6 +2,10 @@ load('api_timer.js');
 load('api_config.js');
 load('api_httplib.js');
 load('api_sys.js');
+load('api_bme280.js');
+
+let myBME = BME280.createI2C(0x76);
+let myBMEData = BME280Data.create();
 
 function timer_handler() {
     print('mJS.httlib Example/ Sys.uptime:', Sys.uptime());
@@ -10,7 +14,10 @@ function timer_handler() {
 
     let req = HTTPReq.create(url, HTTPLib.MT_POST, HTTPLib.CT_JSON);
 
-    req.setBody(JSON.stringify({ values: [Sys.uptime(), Sys.free_ram()] }));
+    myBME.readAll(myBMEData);
+
+    print('temperature, humid=', myBMEData.temp(), myBMEData.humid());
+    req.setBody(JSON.stringify({ values: [myBMEData.temp(), myBMEData.humid()] }));
     req.addHeader('X-Imprement', 'mJS');
 
     let res = HTTPLib.send(req);
@@ -20,8 +27,12 @@ function timer_handler() {
         //ヘッダ値取得
         let val = res.getHeaderVal('content-type');
         print('content-type=' + val);
-        //let val2 = res.getHeaderVal('date');
-        //print('date=' + val2);
+
+        //レスポンスにあるデフォルトバッファを使わないでメモリ確保して値取得する例
+        let val2buff = Sys.malloc(64);
+        let val2 = res.getHeaderValWithBuff('date', val2buff);
+        print('Date=' + val2);
+        Sys.free(val2buff);
 
         // body
         let body = res.getBody();
